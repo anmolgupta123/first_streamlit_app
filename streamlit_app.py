80ed6f30095fd0791551bca85892cc9c318480f6
anmolgupta123 <119111939+anmolgupta123@users.noreply.github.com>
2022-12-02 22:38:38 +0530

    Update streamlit_app.py
    Added Few Other Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
 
 import streamlit
-streamlit.title('Hello ! My name is Anmol Gupta')
+streamlit.title('BREAKFAST MENU FOR CUSTOMERS')
+
+streamlit.header('NORTH INDIAN')
+
+streamlit.header('Food')
+streamlit.text('Mix Parantha/Cheese Parantha/Gobi Parantha')
+
+
+streamlit.header('Beverages')
+streamlit.text('Pepsi/Coke Diet/Thums-Up')
+streamlit.text('Tea/Coffee')
+
+
